2c8ecda850add6ccad8c0ed9d667b5c5d572e2fb
Ajene <ajenec@gmail.com>
2025-03-18 13:53:59 -0400

    do components

---
+++ retro-play/src/components/GameList.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import fetchData from "../adapters";
+
+const GetGames = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+        try{
+            const [result, err]= await fetchData()
+        }
+    }
+  })
+};
