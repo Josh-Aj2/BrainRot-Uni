332812dd57258c7f523da65c2257a69195bc6c93
Ajene <ajenec@gmail.com>
2025-03-19 15:11:40 -0400

    error fixed

--- retro-play/src/adapters/fetchdata.js
+++ retro-play/src/adapters/fetchdata.js
@@ -1,31 +1,3 @@
-// const fetchData = async (url, options = {}) => {
-//   try {
-//     const response = await fetch(url, options);
-
-//     // Throw an error if the response was not 2xx
-//     if (!response.ok)
-//       throw new Error(
-//         `Fetch failed. ${response.status} ${response.statusText}`
-//       );
-
-//     // Check the content type to determine how to parse the response
-//     const contentType = response.headers.get("content-type");
-//     if (contentType && contentType.includes("application/json")) {
-//       const jsonData = await response.json();
-//       return [jsonData, null];
-//     } else {
-//       const textData = await response.text();
-//       return [textData, null];
-//     }
-//   } catch (error) {
-//     // if there was an error, log it and return a tuple: [data, error]
-//     console.error(error.message);
-//     return [null, error];
-//   }
-// };
-
-// export default fetchData;
-
 const fetchData = async (url, options = {}, timeout = 5000) => {
   const controller = new AbortController();
   const timeoutId = setTimeout(() => controller.abort(), timeout);
