85493dc0081a4b79cea75b980abe3d987a50a701
Joshua Young <youngjoshua248@icloud.com>
2025-03-21 23:17:29 -0400

    Second page updated

--- retro-play/src/App.jsx
+++ retro-play/src/App.jsx
@@ -2,6 +2,7 @@
 // import "./App.css";
 // import { Link } from "react-router-dom";
 
+1;
 import "./App.css";
 import SecondPage from "./components/SecondPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -26,3 +27,33 @@ function App() {
 }
 
 export default App;
+
+//2
+// import { Link, useNavigate } from "react-router-dom";
+// import "./App.css";
+
+// function SecondPage() {
+//   const navigate = useNavigate(); // Hook for navigation
+
+//   return (
+//     <div className="second-page-container">
+//       <div className="glass-container">
+//         <h1>Welcome to Page 2</h1>
+
+//         <div className="button-container">
+//           {/* Go Back to Page 1 using <Link> */}
+//           <Link to="/" className="back-link">
+//             Go Back to Page 1
+//           </Link>
+
+//           {/* Go Back to Previous Page using navigate(-1) */}
+//           <button className="back-button" onClick={() => navigate(-1)}>
+//             Go Back
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SecondPage;
--- retro-play/src/components/SecondPage.jsx
+++ retro-play/src/components/SecondPage.jsx
@@ -1,12 +1,13 @@
+1;
 import { Link } from "react-router-dom";
 
 function SecondPage() {
   return (
-    <div>
+    <div className="second-page-container">
       <h1>Welcome to Page 2</h1>
-      <div style={{ textAlign: "center", marginTop: "20px" }}>
-        <Link to="/" style={{ fontsize: "24px", textDecoration: "none" }}>
-          Go back
+      <div className="button-container">
+        <Link to="/" className="back-link">
+          Go Back
         </Link>
       </div>
     </div>
@@ -14,3 +15,47 @@ function SecondPage() {
 }
 
 export default SecondPage;
+
+//2
+// import { Link } from "react-router-dom";
+// import "./secondPage.css"; // Make sure to link your CSS file
+
+// function SecondPage() {
+//   return (
+//     <div className="page-container">
+//       <div className="glass-container">
+//         <h1>Welcome to Page 2</h1>
+//         <Link to="/" style={{ fontSize: "24px", textDecoration: "none" }}>
+//           Go back
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SecondPage;
+
+//3
+// import { Link } from "react-router-dom";
+// import "./app.css"; // Ensure your CSS file is imported
+
+// function SecondPage() {
+//   return (
+//     <div className="second-page-container">
+//       <div className="glass-container">
+//         <h1>Welcome to Page 2</h1>
+
+//         <div className="button-container">
+//           <Link to="/" className="back-link">
+//             Go Back to Home
+//           </Link>
+//           <Link to="/page1" className="back-link">
+//             Go Back to Page 1
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SecondPage;
